8a3228d2da1595dfe60fb214e31d8ca7ad2db6e6
Natalia_Solomonova <Natriy26@gmail.com>
2016-06-01 16:33:55 +0300

    print numbers in direct order

--- HomeWorks/src/HW_1/HW_Cl_1.java
+++ HomeWorks/src/HW_1/HW_Cl_1.java
@@ -3,8 +3,12 @@ package HW_1;
 public class HW_Cl_1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		int a;
+		a=1+2+3+4+5+6+7+8+9+10;
+		System.out.println(a);
+		
+		System.out.println("10 9 8 7 6 5 4 3 2 1");
+		
 	}
 
 }
