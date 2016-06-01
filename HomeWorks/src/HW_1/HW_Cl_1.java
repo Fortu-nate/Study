6362eb6439029c22b479d3096f78774f99a78faf
Natalia_Solomonova <Natriy26@gmail.com>
2016-06-01 16:35:45 +0300

    print the numbers in reverse order

--- HomeWorks/src/HW_1/HW_Cl_1.java
+++ HomeWorks/src/HW_1/HW_Cl_1.java
@@ -7,7 +7,7 @@ public class HW_Cl_1 {
 		a=1+2+3+4+5+6+7+8+9+10;
 		System.out.println(a);
 		
-		System.out.println("10 9 8 7 6 5 4 3 2 1");
+		System.out.println("1 2 3 4 5 6 7 8 9 10");
 		
 	}
 
