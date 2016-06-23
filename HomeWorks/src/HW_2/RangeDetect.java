26f51371947ba2202cf8b0a474b613e61f9f6a63
Natalia_Solomonova <Natriy26@gmail.com>
2016-06-23 11:21:52 +0300

    RangeDetect using else if

---
+++ HomeWorks/src/HW_2/RangeDetect.java
@@ -0,0 +1,20 @@
+package HW_2;
+
+public class RangeDetect {
+
+	public static void main(String[] args) {
+
+		int x = Integer.parseInt(args[0]);
+		
+		if (x == 1)
+			System.out.println("в самый раз");
+		else if (x == 2)
+			System.out.println("много");
+		else if (x == 5)
+			System.out.println("мало");
+		else
+			System.out.println("задайте другое число");
+		
+	}
+
+}
