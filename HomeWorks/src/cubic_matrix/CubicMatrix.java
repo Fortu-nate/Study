a6da5b875f8bfe55e3057bef2518bb804e6927f9
Natalia_Solomonova <Natriy26@gmail.com>
2016-07-20 19:43:18 +0300

    print number of layer with 0

--- HomeWorks/src/cubic_matrix/CubicMatrix.java
+++ HomeWorks/src/cubic_matrix/CubicMatrix.java
@@ -7,24 +7,29 @@ public class CubicMatrix {
 		int[][][] matrix = new int[2][2][2];
 		// first layer
 		matrix[0][0][0] = 1;
-		matrix[0][0][1] = 2;
+		matrix[0][0][1] = 0;
 		matrix[0][1][0] = 3;
 		matrix[0][1][1] = 4;
 
 		// second layer
 		matrix[1][0][0] = 5;
-		matrix[1][0][1] = 6;
-		matrix[1][1][0] = 7;
+		matrix[1][0][1] = 0;
+		matrix[1][1][0] = 0;
 		matrix[1][1][1] = 8;
 
-		for (int row = 0; row < matrix.length; row++) {
-			for (int column = 0; column < matrix[row].length; column++) {
-				for (int layer = 0; layer < matrix[column].length; layer++) {
-					System.out.print(matrix[row][column][layer] + " ");
+		OUTER: for (int layer = 0; layer < matrix.length; layer++) {
+			for (int column = 0; column < matrix[layer].length; column++) {
+				for (int row = 0; row < matrix[column].length; row++) {
+					if (matrix[layer][column][row] == 0) {
+						System.out.println(layer);
+						continue OUTER;
+					}
+
+					// System.out.print(matrix[row][column][layer] + " ");
 				}
-				System.out.println();
+				// System.out.println();
 			}
-			System.out.println();
+			// System.out.println();
 		}
 	}
 
