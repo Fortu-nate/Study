f75da245caa5e8a28f805c83f8842766b96a2cb8
Natalia_Solomonova <Natriy26@gmail.com>
2016-07-21 10:56:45 +0300

    Print layer with 0 (short form)

--- HomeWorks/src/cubic_matrix/CubicMatrix.java
+++ HomeWorks/src/cubic_matrix/CubicMatrix.java
@@ -4,18 +4,18 @@ public class CubicMatrix {
 
 	public static void main(String[] args) {
 
-		int[][][] matrix = new int[2][2][2];
+		int[][][] matrix = { { { 1, 0 }, { 3, 4 } }, { { 5, 0 }, { 0, 8 } } };
 		// first layer
-		matrix[0][0][0] = 1;
-		matrix[0][0][1] = 0;
-		matrix[0][1][0] = 3;
-		matrix[0][1][1] = 4;
+		// matrix[0][0][0] = 1;
+		// matrix[0][0][1] = 0;
+		// matrix[0][1][0] = 3;
+		// matrix[0][1][1] = 4;
 
 		// second layer
-		matrix[1][0][0] = 5;
-		matrix[1][0][1] = 0;
-		matrix[1][1][0] = 0;
-		matrix[1][1][1] = 8;
+		// matrix[1][0][0] = 5;
+		// matrix[1][0][1] = 0;
+		// matrix[1][1][0] = 0;
+		// matrix[1][1][1] = 8;
 
 		OUTER: for (int layer = 0; layer < matrix.length; layer++) {
 			for (int column = 0; column < matrix[layer].length; column++) {
@@ -25,7 +25,7 @@ public class CubicMatrix {
 						continue OUTER;
 					}
 
-					// System.out.print(matrix[row][column][layer] + " ");
+					// System.out.print(matrix[layer][column][row] + " ");
 				}
 				// System.out.println();
 			}
